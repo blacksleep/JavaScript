205da250e27378a4a6313514485271fd4f846b7b
Josh Waggoner <joshua.j.waggoner@gmail.com>
2014-10-21 20:50:47 -0600

    First commit; created bricks.js and loop.js

--- bricks.js
+++ bricks.js
@@ -1,6 +1,7 @@
 /**
  * Created by BlackSleep on 10/16/2014.
  */
+
 function mason(small, large, total_length) {
     var totalSmall = small;
     var totalLarge = large * 5;
@@ -13,3 +14,12 @@ function mason(small, large, total_length) {
 }
 
 console.log (mason(3,5,100))
+
+//shortcut version below
+
+function mason(sm, lg, len){
+    return (sm + (lg * 5) >= len)? true:false;  //The "?" acts as "if '(sm + (lg *5) >= len)' is true, return 'true'" and the ":" acts as "if '(sm + (lg * 5) >= len)' is false, return 'false'"
+}
+
+console.log (mason(3,5,100))
+
--- loop.js
+++ loop.js
@@ -20,3 +20,18 @@ return longest;
 }
 
 console.log(findLongestWord("What is the longest word in the sentence?"))
+
+//10/20/14 notes
+
+//looping through an array
+
+arr forEach(function(item; index)) {  //this "arr forEach()"  method that comes with all arrays, and executes a provided function                                        //once per array element,it will do a "for (int;____;____)" sequence
+}
+//or
+
+function output(item) {
+    console.log(item);
+}
+
+arr forEach(output)
+
